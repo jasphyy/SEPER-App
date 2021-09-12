d388d244420d9827e07b110c76f202ac474d18eb
JaspherVillanueva <69949305+JaspherVillanueva@users.noreply.github.com>
2021-09-12 18:54:17 +1200

    fixed root npm init problem

---
+++ models/Article.js
@@ -0,0 +1,48 @@
+// models/Article.js
+
+const mongoose = require('mongoose');
+
+const ArticleSchema = new mongoose.Schema({
+  id: {
+      type: Number,
+      required: true
+  },
+  
+title: {
+    type: String,
+    required: true
+  },
+  
+  authors: {
+    type: String,
+    required: true
+  },
+
+  source: {
+    type: String,
+    required: true
+  },
+
+  published_year: {
+    type: Date,
+    required: true
+  },
+
+  doi: {
+    type: String
+  },
+
+  claim: {
+    type: String,
+    required: true
+  },
+
+  evidence_level: {
+      type: String,
+      required: true
+  }
+
+
+});
+
+module.exports = Article = mongoose.model('article', ArticleSchema);
