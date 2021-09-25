276c0b1678ea3801877e6e6b34f7bab5b463148f
JaspherVillanueva <69949305+JaspherVillanueva@users.noreply.github.com>
2021-09-25 16:46:01 +1200

    updating dropdown

--- frontend/src/pages/SEPractice.js
+++ frontend/src/pages/SEPractice.js
@@ -2,6 +2,7 @@ import articles from "../dummydata/articles.js";
 import Styles from "../components/tablestyle.js";
 import Table from "../components/evidencetable.js";
 import tablecolumns from "../components/tablecolumns.js";
+import Dropdown from "../components/Dropdown.js";
 
 const SEPractice = () => {
     return (
@@ -9,6 +10,7 @@ const SEPractice = () => {
         <h2>Select SE Practice to get evidence for the claimed benefits</h2>
         <p>There will be a way to select an SE practice from our SEPER repository here</p>
         <p>Also the display table of evidence levels for different claims will show here for the SE practice selected</p>
+        <Dropdown/>
                <Styles>
                  <Table
                   data={articles}
