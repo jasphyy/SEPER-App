c95c225d4958de92da62712ec84b7c1a10742e68
JaspherVillanueva <69949305+JaspherVillanueva@users.noreply.github.com>
2021-10-11 04:07:49 +1300

    mongoDB fix

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      db,
+      process.env.MONGODB_CONNECTION_STRING,
       {
         useNewUrlParser: true
       }
