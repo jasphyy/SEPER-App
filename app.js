589ff125c70a5a993869acce574b117422087a35
JaspherVillanueva <69949305+JaspherVillanueva@users.noreply.github.com>
2021-10-10 20:42:02 +1300

    heroku-deploy

--- app.js
+++ app.js
@@ -22,4 +22,8 @@ app.use("/api/articles", articles);
 app.use(cors());
 app.use(express.json);
 
+if (process.env.NODE_ENV === 'production'){
+    app.use(express.static('frontend/build'));
+}
+
 app.listen(port, () => console.log(`Server running on port ${port}`));
