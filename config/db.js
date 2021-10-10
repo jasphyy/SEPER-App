f12ebd13bb39d5cd9fe45ff131aeda498db702fb
JaspherVillanueva <69949305+JaspherVillanueva@users.noreply.github.com>
2021-10-11 02:43:00 +1300

    deploy please

--- config/db.js
+++ config/db.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      db,
+      process.env.MONGODB_CONNECTION_STRING,
       {
         useNewUrlParser: true
       }
