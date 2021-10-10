5a3b7f4be604683142981a36b82441f9a84587de
JaspherVillanueva <69949305+JaspherVillanueva@users.noreply.github.com>
2021-10-10 21:11:38 +1300

    retry heroku deployment

--- app.js
+++ app.js
@@ -27,7 +27,10 @@ const path = require("path");
 
 
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'));
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.get('*', (req,res) => {
+      res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'))
+    })
 
   
 }
