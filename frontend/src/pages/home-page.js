58663d72e34c346eac12a2bac8caab9152eeb81a
JaspherVillanueva <69949305+JaspherVillanueva@users.noreply.github.com>
2021-10-11 03:56:37 +1300

    added how to use

--- frontend/src/pages/home-page.js
+++ frontend/src/pages/home-page.js
@@ -6,7 +6,17 @@ class HomePage extends Component {
       <div class="container pt-3 border border-dark border-top-0 rounded-bottom bg-light">
         <h2> How to use SEPER </h2>
         <hr></hr>
-        <p> To use the SEPER app ....</p>
+        <p>
+          - To view all articles in SEPER, click the “Select the Practice”
+          button in the menu <br></br>
+          - To find articles by topic, title, author, or
+          publication date, click the “Select the Practice” button in the menu
+          and place a query in the search bar to view SEPER approved articles,
+          followed by clicking the "search" button. <br></br>
+          - To submit an article
+          recommendation for SEPER, click the “Submit an Article” tab in the
+          menu and fill out the required details in the form displayed.
+        </p>
       </div>
     );
   }
