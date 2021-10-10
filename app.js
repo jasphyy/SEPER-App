835f708bb872d2fe6c67702a4b1d98adecdaa4f9
JaspherVillanueva <69949305+JaspherVillanueva@users.noreply.github.com>
2021-10-10 20:49:43 +1300

    trying deployment

--- app.js
+++ app.js
@@ -21,9 +21,18 @@ app.use("/api/articles", articles);
 
 app.use(cors());
 app.use(express.json);
+app.use(express.static('frontend/build'));
 
-if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'));
-}
+const path = require("path");
+
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+});
+
+// if (process.env.NODE_ENV === 'production'){
+    
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
