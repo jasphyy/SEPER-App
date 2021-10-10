6dafaa17cd751f9ff523e2bd3dffb068c6f1d81d
JaspherVillanueva <69949305+JaspherVillanueva@users.noreply.github.com>
2021-10-11 00:33:17 +1300

    deploy heroku

--- app.js
+++ app.js
@@ -20,14 +20,11 @@ app.use(bodyparser.json());
 //use Routes
 app.use("/api/articles", articles);
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(frontend/build));
-    
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+app.use(express.static(path.resolve(__dirname, "./frontend/build")));
 
-    })
-}
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./frontend/build", "index.html"));
+});
 
 const port = process.env.PORT || 3000;
 
